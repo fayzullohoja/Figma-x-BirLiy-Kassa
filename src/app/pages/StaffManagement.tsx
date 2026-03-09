4cd84b461a2694f2a8a123de4fd9bf5e9d71084a
fayzullohoja <76996390+fayzullohoja@users.noreply.github.com>
2026-03-09 22:13:18 +0500

    Update StaffManagement.tsx

--- src/app/pages/StaffManagement.tsx
+++ src/app/pages/StaffManagement.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { Header } from '../../components/layout/Header';
-import { Navigation } from '../../components/layout/Navigation';
-import { SubscriptionGuard } from '../../components/layout/SubscriptionGuard';
+import { Header } from '../components/Header';
+import { Navigation } from '../components/Navigation';
+import { SubscriptionGuard } from '../components/SubscriptionGuard';
 import { mockWaiters } from '../../lib/data/mockData';
 import { Waiter } from '../../types';
 import { Plus, UserCheck, UserX } from 'lucide-react';
