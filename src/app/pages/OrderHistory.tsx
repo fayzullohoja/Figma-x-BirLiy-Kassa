f5b677e0592caa28ddbc7946acab81b0e498597a
fayzullohoja <76996390+fayzullohoja@users.noreply.github.com>
2026-03-09 22:18:07 +0500

    Update OrderHistory.tsx

--- src/app/pages/OrderHistory.tsx
+++ src/app/pages/OrderHistory.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Header } from '../../components/layout/Header';
-import { Navigation } from '../../components/layout/Navigation';
-import { SubscriptionGuard } from '../../components/layout/SubscriptionGuard';
+import { Header } from '../../components/Header';
+import { Navigation } from '../../components/Navigation';
+import { SubscriptionGuard } from '../../components/SubscriptionGuard';
 import { Clock, User } from 'lucide-react';
 import { formatCurrency } from '../../lib/utils/format';
 import { Link, useNavigate } from 'react-router';
